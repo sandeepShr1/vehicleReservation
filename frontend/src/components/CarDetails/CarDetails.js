52af18341ea2046eef1e274e92663b1de0061764
sandeepshrestha2056 <sandeepshrestha2056@gmail.com>
2023-03-25 22:40:31 +0545

    added review and rating

--- frontend/src/components/CarDetails/CarDetails.js
+++ frontend/src/components/CarDetails/CarDetails.js
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { connect } from "react-redux";
+import { connect, useDispatch } from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
-import { getCarDetails } from "../../redux/actions/carActions";
+import { clearError, getCarDetails, newReview } from "../../redux/actions/carActions";
 import Loading from "../Loading/index";
 import styled from "styled-components";
 import DateRangeComp from "../DateRange/DateRangeComp";
+import StarBorderIcon from "@mui/icons-material/StarBorder"
+
+import { Rating } from "@mui/material";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button,
+} from "@mui/material";
+import ReviewCard from "../ReviewRating";
+import { NEW_REVIEWS_RESET } from "../../redux/constants/carConstants";
+import { toast } from "react-hot-toast";
 
 const CarDetail = styled.div`
   width: 100%;
@@ -46,6 +59,13 @@ const CarDetail = styled.div`
         flex-direction: column;
         gap: 1.5rem;
         padding: 2rem 0;
+        .__reviews {
+          color: #f02d34;
+          margin-top: 10px;
+          display: flex;
+          gap: 5px;
+          align-items: center;
+        }
         :first-child {
           li {
             list-style: none;
@@ -67,36 +87,148 @@ const CarDetail = styled.div`
     }
   }
   .__book {
-    .__book_btn {
-      margin: 2rem 0;
-      padding: 1.6rem 2rem;
-      border: none;
-      background: #ed143d;
-      border-radius: 9px;
-      color: #ffffff;
-      font-size: 1.6rem;
-      cursor: pointer;
-      :hover {
-        background: #c03952;
+    .__btns{
+      display: flex;
+      gap: 2rem;
+
+      .__book_btn {
+        margin: 2rem 0;
+        padding: 1.6rem 2rem;
+        border: none;
+        background: #ed143d;
+        border-radius: 9px;
+        color: #ffffff;
+        font-size: 1.6rem;
+        cursor: pointer;
+        :hover {
+          background: #c03952;
+        }
       }
     }
+    
+  }
+
+  .__submitReview {
+    display: flex ;
+    flex-direction: column;
+    background-color: #f02d34;
+
+    .submitDialogTextArea {
+    border: 1px solid rgba(0, 0, 0, 0.082);
+    margin: 1vmax 0;
+    outline: none;
+    padding: 1rem;
+    font: 300 1rem;
+  }
+  }
+
+  
+
+  .reviewsHeading {
+    color: #000000be;
+    font: 500 1.4vmax;
+    text-align: center;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.226);
+    padding: 1vmax;
+    width: 20vmax;
+    margin: auto;
+    margin-bottom: 4vmax;
+  }
+
+  .reviews {
+    display: flex;
+    overflow: auto;
+  }
+
+  .reviewCard {
+    flex: none;
+
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.226);
+    /* border: 1px solid rgba(56, 56, 56, 0.116); */
+    width: 10vmax;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 1vmax;
+    padding: 3vmax;
+  }
+
+  .reviewCard > img {
+    width: 5vmax;
+  }
+
+  .reviewCard > p {
+    color: rgba(0, 0, 0, 0.836);
+    font: 600 0.9vmax;
+  }
+
+  .reviewCardComment {
+    color: rgba(0, 0, 0, 0.445);
+    font: 300 0.8vmax;
+  }
+
+  .noReviews {
+    font: 400 1.3vmax "Gill Sans";
+    text-align: center;
+    color: rgba(0, 0, 0, 0.548);
   }
 `;
 
-const CarDetails = ({ getCarDetails, car, loading, error }) => {
+const CarDetails = ({ getCarDetails, car, loading, error, newReview, success,
+  reviewError }) => {
   const [rangeDates, setRangeDates] = useState([]);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
+  const [open, setOpen] = useState(false);
+
   const { id } = useParams();
   const history = useNavigate();
+  const dispatch = useDispatch();
 
   const handleBooking = () => {
     const items = { car: car, rangeDates: rangeDates };
     localStorage.setItem("booking", JSON.stringify(items));
     history("../booking/new", { replace: true });
   };
+  const submitReviewToggle = () => {
+    open ? setOpen(false) : setOpen(true);
+  };
+  const submitReviewHandler = () => {
+    const myForm = new FormData();
+
+    myForm.set("rating", rating);
+    myForm.set("comment", comment);
+    myForm.set("carId", id);
+
+    newReview(myForm);
+    setOpen(false);
+  };
+  const options = {
+    size: "large",
+    value: car?.ratings,
+    readOnly: true,
+    precision: 0.5,
+  };
+
 
   useEffect(() => {
+    if (error) {
+      toast.error(error);
+      clearError();
+    }
+    if (reviewError) {
+      alert.error(reviewError);
+      dispatch(clearError());
+    }
+
+    if (success) {
+      toast.success("Review Submitted Successfully");
+      dispatch({ type: NEW_REVIEWS_RESET });
+    }
     getCarDetails(id);
-  }, []);
+
+
+  }, [dispatch, toast, error, reviewError, success]);
   if (loading) {
     return <Loading />;
   }
@@ -117,6 +249,7 @@ const CarDetails = ({ getCarDetails, car, loading, error }) => {
             <li>Vehicle Type</li>
             <li>Number Plate</li>
             <li>Rate</li>
+            <li>Rating</li>
           </ul>
           <ul>
             <li>{car?.name || "-"}</li>
@@ -125,27 +258,85 @@ const CarDetails = ({ getCarDetails, car, loading, error }) => {
             <li>{car?.vehicleType || "-"}</li>
             <li>{car?.numberPlate || "-"}</li>
             <li>Rs. {car?.price || "-"} per day</li>
+            <li>
+              <div className="__reviews">
+                <div>
+                  <Rating {...options} />
+                </div>
+                <p>({car?.numOfReviews})</p>
+              </div>
+            </li>
           </ul>
         </div>
         <div className="__book">
           <DateRangeComp setRangeDates={setRangeDates} />
 
-          <button className="__book_btn" onClick={handleBooking}>
-            Book Now
-          </button>
+          <div className="__btns">
+            <button className="__book_btn" onClick={handleBooking}>
+              Book Now
+            </button>
+            <button
+              onClick={submitReviewToggle}
+              className="__book_btn"
+              type='button'
+            >
+              Submit Review
+            </button>
+          </div>
         </div>
       </div>
+      <h3 className="reviewsHeading">REVIEWS</h3>
+      <Dialog
+        aria-labelledby="simple-dialog-title"
+        open={open}
+        onClose={submitReviewToggle}
+      >
+        <DialogTitle>Submit Review</DialogTitle>
+        <DialogContent className="__submitReview" style={{ display: "flex", flexDirection: "column" }}>
+          <Rating
+            onChange={(e) => setRating(e.target.value)}
+            value={Number(rating)}
+            size="large"
+            name="size-large"
+            sx={{ fontSize: 14, background: "red" }}
+          />
+          <textarea
+            className="submitDialogTextArea"
+            cols="30"
+            rows="5"
+            onChange={(e) => setComment(e.target.value)}
+          ></textarea>
+          <DialogActions>
+            <Button color="secondary" onClick={submitReviewToggle}>
+              Cancel
+            </Button>
+            <Button color="primary" onClick={submitReviewHandler}>
+              Submit
+            </Button>
+          </DialogActions>
+        </DialogContent>
+      </Dialog>
+      {car.reviews && car.reviews[0] ? (
+        <div className="reviews">
+          {car.reviews && car.reviews.map((review) => <ReviewCard key={review._id} review={review} />)}
+        </div>
+      ) : (
+        <p className="noReviews">No reviews</p>
+      )}
     </CarDetail>
   );
 };
-const mapStateToProps = ({ carDetails: { loading, car, error } }) => ({
+const mapStateToProps = ({ carDetails: { loading, car, error }, newReview: { success, error: reviewError } }) => ({
   loading,
   car,
   error,
+  success,
+  reviewError
 });
 
 const mapDispatchToProps = {
   getCarDetails,
+  newReview,
 };
 
 const connector = connect(mapStateToProps, mapDispatchToProps);
---
+++ frontend/src/components/ReviewRating.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Rating } from '@mui/material';
+// import profilePng from "../../images/Profile.png"
+
+const ReviewCard = ({ review }) => {
+      const options = {
+            size: "large",
+            value: review.rating,
+            readOnly: true,
+            precision: 0.5,
+
+
+      }
+      console.log(review)
+      return (
+            <div className="reviewCard">
+                  {/* <img src={profilePng} alt="User" /> */}
+                  <p>{review.name}</p>
+                  <Rating {...options} />
+                  <span>{review.comment}</span>
+            </div>
+      )
+}
+
+export default ReviewCard;
